6493b6e0cd327b3839bc8ee42783921b571277d9
long-jinhui <320235708@qq.com>
2020-12-08 21:25:49 +0800

    修改

--- 006.整形数据/无符号短整型（错误示范）.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
- unsigned short a = -1;    //定义a为无符号短整型变量   不能存复数  -1转补码转二进制 再转“%d”格式输出 
-	printf("%d\n", a);      //指定用有符号十进制数格式输出     
-	return 0;
- } 
- 
--- 006.整形数据/无符号长整形.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	unsigned long a=222;
-	printf("%u",a);
-	return 0;
- } 
