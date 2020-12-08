eeae46c4fa428ecf175721c44dbfb5f5a3b60bcc
long-jinhui <320235708@qq.com>
2020-12-08 11:34:43 +0800

    修改

---
+++ 006.整形数据/无符号长整形.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	unsigned long a=222;
+	printf("%u",a);
+	return 0;
+ } 
---
+++ 006.整形数据/无符号长整形（错误示范）.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	unsigned long a=-222; 
+	printf("%d",a);   //%d输出有符号 但因为long范围够大 所以不会出问题     输出-222 
+	return 0;
+ } 
