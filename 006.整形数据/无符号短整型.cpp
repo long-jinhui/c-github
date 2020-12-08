65a52907283c74278e52cfdae00deebee9b98981
long-jinhui <320235708@qq.com>
2020-12-08 21:42:09 +0800

    修改

--- 006.整形数据/无符号短整型.cpp
+++ 006.整形数据/无符号短整型.cpp
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
-{
-	unsigned short a = 50 ;    //定义a为无符号短整型变量 
+{                                  //因为无符号和有符号整数在内存中都是用补码形式存储的      //而要存 负数（-10）应先将其转换成补码 
+	unsigned short a = 1 ;    //定义a为无符号短整型变量 
 	printf("%u\n",a);           //指定用无符号十进制数格式输出 
-	return 0;
- } 
+	return 0;                   /*用%u打印无符号类型                    用%d会把无符号类型转换成有符号类型输出   用%d就会把unsigned 转换成signed再输出*/
+	    
+ } //注意unsigned short取值范围 0~65535 
  
