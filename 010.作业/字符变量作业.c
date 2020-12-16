37ea9d2fbd310b9f426cfced1c9fc448b0448cc1
longjinhui <320235708@qq.com>
2020-12-16 22:32:44 +0800

    修改

---
+++ 010.作业/字符变量作业.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char c1,c2;                         //定义 字符变量 
+	c1=97;c2=98;                   //c1=197;c2=198;
+ 	printf("%c   %c\n",c1,c2);       //?   ?     ASCⅡ代码 
+ 	printf("%d   %d\n",c1,c2);       //-59   -58  
+	system("PAUSE"); 
+ } 
---
+++ 010.作业/输入作业.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	float x,y;
+	char c1,c2;
+	scanf("%d,%d",&a,&b);
+	printf("%d",a);
+	printf("%d",b);
+
+	scanf("%f  %e",&x,&y);
+	printf("%f",x);
+	printf("%e",y);
+
+
+	scanf("%c,%c",&c1,&c2);
+	printf("%c", c2);
+	printf("456564456"); 
+	printf("\n");
+	printf("%c",c1);
+	printf("%c",c2);
+
+	return 0;
+ }  
