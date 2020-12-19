5ff5f9ffa8a5c3a5d8e56b312dfe02c313df6c58
longjinhui <320235708@qq.com>
2020-12-19 16:27:43 +0800

    xiugai

---
+++ 005.混日子/赋值+if（水）.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a,b,c;
+   	scanf("%d%d",&a,&b);
+   	c=a+b;
+   	if(c>0)
+   	printf("%d",c);
+   	else{
+	   printf("you are so fucking stupid just get out,please!");
+	   }
+	system("pause");
+ 	return 0;
+ } 
