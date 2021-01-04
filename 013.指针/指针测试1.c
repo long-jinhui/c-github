fbdc1519282bfeabfa21dee1f995cf451f171f32
longjinhui <320235708@qq.com>
2021-01-04 22:43:01 +0800

    修改

--- 013.指针/指针测试1.c
+++ 013.指针/指针测试1.c
@@ -2,9 +2,11 @@
 int main()
 {
 	int a=1;
-	int * ptr=&a;  /*定义 ptr 为 int 的类型  名称为ptr*/ /* int *表示ptr只能存放int类型变量的地址*/ 
+	int * ptr=&a;  /*定义 ptr 为 int * 的类型（存放int变量地址的类型）  名称为ptr*/
+	 /* int* 类型 表示ptr只能存放int类型变量的地址*/ 
+	 /*   ptr只能存放地址*/
 	printf("a的值=%d,a的地址=%p\n",a,&a);
-	printf("ptr地址=%p 存放的值（的地址）=%p  值= %d",&ptr,ptr,*ptr+1);
+	printf("ptr地址=%p 存放的值（的地址）=%p  值= %d",&ptr,ptr,*ptr);
 	getchar();
 	return 0;
- } 
+ } /*   *ptr就是以 ptr的内容（i的地址）  为地址的变量  */ 
--- 013.指针/指针测试2.c
+++ 013.指针/指针测试2.c
@@ -3,9 +3,11 @@ int main()
 {
 	int *p;
 	int i=3;
-	p=&i;   /*p保存了i的地址 p指向i*/
+	p=&i;   /*p保存了i的地址 p指向i*/  	/* p指向啥*p就是啥  */ 
+	/* *p就是i  */ /*   p（指针）只能存放地址*/
 	printf("i的值=%d,i的地址=%p\n",i,&i);
-	printf("ptr地址=%p 存放的值（的地址）=%p  值= %p",&p,p,*p);
+	printf("p地址=%p 存放的值（的地址）=%p  值= %d",&p,p,*p);
 	getchar();
 	return 0;
  } 
+ /*   *p就是以 p的内容（i的地址）  为地址   的变量  */ /* *p就是i  */
