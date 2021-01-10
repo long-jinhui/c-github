77ff32fa2c1794aa78bacb8fb1125a7333a8955c
longjinhui <320235708@qq.com>
2021-01-10 20:36:10 +0800

    修改

--- 015.for 循环/for循环.c
+++ 015.for 循环/for循环.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
- 
+#include <stdlib.h>
 int main ()
 {
-	int a;
-   /* for 循环执行 */
-      for(a=10; a<20;a=a+1)
-   {
-      printf("a 的值： %i\n", a);
-   }
-   getchar(); 
-   return 0;
+	int a,b=0;
+	scanf("%d",&a);
+      for(; a<=100;a++)  /*for语句括号后面的第一个;为执行语句*/
+	{                       /*要执行两个或以上语句要加大括号*/ 
+	  	  b=b+a;           /*此处若不加 {} 则只 输出 一次b的值 */ 
+      printf("b的值： %i\n", b); /*但是b=b+a; 会一直执行*/ 
+	}
+system("pause");
+	return 0; 
 }
