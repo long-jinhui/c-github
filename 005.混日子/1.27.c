d750a367e8a879b67acda71b6ebc2f139e84d964
longjinhui <320235708@qq.com>
2021-01-27 01:37:30 +0800

    修改

---
+++ 005.混日子/1.27.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+     int g;
+     scanf("%i",&g);
+ 	 switch(g)
+ 	 {
+ 	 	case 123 : printf("aaa\n\n");break; /*凌晨1点半 楼下酒吧鬼死甘吵 真系请你好好放低*/
+		case 456 : printf("bb\n\n");break;  /*  ''这个框是给字母的 数字不用 */ 
+		case 789 : printf("cc\n\n");break;
+		case 666 : printf("d\n\n");break;
+		default  : printf("凡事想想自己配不配\n");return 0;
+		  	  }
+    return main();
+}
