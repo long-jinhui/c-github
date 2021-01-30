9d9b0db07fd2af91c3e7f9e2c2237a4003bf41b2
longjinhui <320235708@qq.com>
2021-01-31 00:10:32 +0800

    修改

--- 017数组/数组作业.c
+++ 017数组/数组作业.c
@@ -2,13 +2,13 @@
 int main()
 {   
 	int i=0;
-	char a[26];
-	a[0]='A';
+	char a[26];   /*定义数组类型为字母*/
 	for(;i<=25;i++)
-	{   
-		printf("%d",i);
+	{
+		//printf("%d",i);
 		a[i]='A'+i;  
-		printf("%c\n",a[i]);
+		printf("a[%d]=%c\n",i,a[i]);
+		//printf("%c\n",a[i]);
 	}
 	getchar();
 }
